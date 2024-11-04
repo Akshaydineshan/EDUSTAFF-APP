9b75ae11a357497239bb5cd851e538df205a55d6
akshaymv <Akshaymv.prm@gmail.com>
2024-11-04 09:37:36 +0530

    table document count update

--- src/app/modules/teachers/teacher-list.component.ts
+++ src/app/modules/teachers/teacher-list.component.ts
@@ -64,9 +64,9 @@ export class TeacherListComponent implements OnInit {
   teacherId!: any;
   schoolId!: any;
 
-  API_BASE_IMAGE:any=environment.imageBaseUrl
+  API_BASE_IMAGE: any = environment.imageBaseUrl
 
-  constructor(private fb: FormBuilder, private dataService: DataService,private router:Router,private toastr: ToastrService) {
+  constructor(private fb: FormBuilder, private dataService: DataService, private router: Router, private toastr: ToastrService) {
     this.filterForm = this.fb.group({
       subjectFilter: [''],
       retiringInMonths: [],
@@ -108,13 +108,13 @@ export class TeacherListComponent implements OnInit {
 
     // Check right edge
     if (this.mouseX + popupWidth > window.innerWidth) {
-    
+
       this.mouseX = window.innerWidth - popupWidth - offset; // Position left
     }
 
     // Check bottom edge
     if (this.mouseY + popupHeight > window.innerHeight) {
-      this.mouseY = event.clientY - popupHeight+20; // Position above the mouse
+      this.mouseY = event.clientY - popupHeight + 20; // Position above the mouse
     }
 
 
@@ -196,26 +196,56 @@ export class TeacherListComponent implements OnInit {
 
     this.dataService.getTeachersData().subscribe(
       (data) => {
+        debugger
         this.teacherList = data.map((teacher: Teacher) => ({
           ...teacher,
           documentStatus: this.getDocumentStatus(teacher.documentCount, teacher.error)
         }));
- 
+        console.log("document sytatus check", this.teacherList)
+
         this.teacherTableRows = this.teacherList
         this.teacherTableColumns = [
-        { field: "name", filter: true, floatingFilter: true,
-          cellRenderer: (params: any) => `<a style="cursor: pointer; color: blue;" target="_blank">${params.value}</a>` },
-        { field: "schoolName", filter: true, floatingFilter: false },
-        { field: "designation", filter: true, floatingFilter: false },
-        { field: "subject", filter: true, floatingFilter: false },
-        { field: "employeeType", filter: true, floatingFilter: false },
-        { field: "experienceYear", filter: true, floatingFilter: false },
-        { field: "age", filter: true, floatingFilter: false },
-        { field: "documentCount", filter: true, floatingFilter: false },
-        { field: "phoneNumber", filter: true, floatingFilter: false },
+          {
+            field: "name", filter: true, floatingFilter: true,
+            cellRenderer: (params: any) => `<a style="cursor: pointer; color: blue;" target="_blank">${params.value}</a>`
+          },
+          { field: "schoolName", filter: true, floatingFilter: false },
+          { field: "designation", filter: true, floatingFilter: false },
+          { field: "subject", filter: true, floatingFilter: false },
+          { field: "employeeType", filter: true, floatingFilter: false },
+          { field: "experienceYear", filter: true, floatingFilter: false },
+          { field: "age", filter: true, floatingFilter: false },
+          { field: "phoneNumber", filter: true, floatingFilter: false },
+      //     {
+      //       field: "documentCount", filter: true, floatingFilter: false,
+      //       cellRenderer: (params: any) => {
+      //         console.log("params",params)
+      //         debugger
+      //         `<span [class]="params.value?.icon ? 'doc-count' : ''">
+      //   <i [class]="params.value?.icon"></i>${params.value?.text}
+      // </span>`
+      //       }
+      //     },
+      {
+        field: "documentStatus", 
+        filter: true, 
+        floatingFilter: false,
+        cellRenderer: (params: any) => {
+          const iconClass = params.value?.icon || '';
+          const text = params.value?.text || '0';
+          const hasIconClass = iconClass ? 'doc-count' : '';
+    
+        
+          return `
+            <span class="${hasIconClass}">
+              <i class="${iconClass}"></i> ${text}
+            </span>`;
+        }
 
+
+      },
         ];
-        console.log(this.teacherList);
+        console.log(this.teacherList,this.teacherTableColumns);
         this.updatePaginatedData();
 
 
@@ -287,7 +317,7 @@ export class TeacherListComponent implements OnInit {
     return Array.from({ length: this.totalPages }, (_, i) => i + 1);
   }
 
- 
+
 
   toggleFilterDropdown() {
     this.showFilterModal = !this.showFilterModal;
@@ -326,7 +356,7 @@ export class TeacherListComponent implements OnInit {
   get getTeacherImage() {
     debugger
     let result = '';
-    
+
     if (this.API_BASE_IMAGE && this.selectedTeacher?.photo && this.selectedTeacher?.photo !== 'null') {
       result = this.API_BASE_IMAGE.replace(/\/+$/, '') + '/' + this.selectedTeacher.photo?.replace(/^\/+/, '');
     }
@@ -335,7 +365,7 @@ export class TeacherListComponent implements OnInit {
   }
 
   onTeacherHover(teacherId: number, teacherData: any, event: MouseEvent): void {
-    this.selectedTeacher=null
+    this.selectedTeacher = null
     this.teacherId = teacherId;
     if (this.hoverTimeout) {
       clearTimeout(this.hoverTimeout);
@@ -351,7 +381,7 @@ export class TeacherListComponent implements OnInit {
         this.dataService.getTeacherDetailPopUp(teacherId).subscribe(
           (data) => {
             this.selectedTeacher = data; // Store the detailed info
-            console.log("teachres",data)
+            console.log("teachres", data)
             if (this.selectedTeacher && teacherId) {
               this.showPopup = true;
               this.updateMousePosition(event);
@@ -361,7 +391,7 @@ export class TeacherListComponent implements OnInit {
             console.error('Error fetching teacher details:', error);
           }
         );
-      },300);
+      }, 300);
     }
   }
 
@@ -379,7 +409,7 @@ export class TeacherListComponent implements OnInit {
         this.dataService.getSchoolDetailPopUp(schoolId).subscribe(
           (data) => {
             this.selectedSchool = data;
-            console.log("school",this.selectedSchool)
+            console.log("school", this.selectedSchool)
             this.showSchoolPopup = true;
             this.updateMousePosition(event);
           },
@@ -419,7 +449,7 @@ export class TeacherListComponent implements OnInit {
     const rowNode: any = event.node;
     const rowData = rowNode.data;
 
-  
+
     if (event.colDef.field === "name") {
       console.log("teacher", event)
       this.onTeacherHover(rowData.teacherId, rowData, event.event)
@@ -430,14 +460,14 @@ export class TeacherListComponent implements OnInit {
   }
   rowMouseHoverOut(event: any) {
     // if (event.colDef.field === "name") {
-      this.onTeacherMouseOut()
+    this.onTeacherMouseOut()
     // } else if (event.colDef.field === "schoolName") {
-      this.onSchoolMouseOut()
+    this.onSchoolMouseOut()
 
     // }
 
   }
-  
+
   get getschoolImage() {
     let result = '';
     if (this.apiUrl && this.selectedSchool?.photo && this.selectedSchool?.photo !== 'null') {
@@ -446,17 +476,17 @@ export class TeacherListComponent implements OnInit {
     // If the result is an empty string, it will fallback to emptyImage in the template
     return result;
   }
-  onCellClicked(event:any){
+  onCellClicked(event: any) {
     debugger
-  
+
     const rowNode: any = event.node;
     const rowData = rowNode.data;
-    let teacherId:number=rowData.teacherId
-    
+    let teacherId: number = rowData.teacherId
+
     if (event.colDef.field === "name") {
-      this.router.navigate(['/teachers/view-teacher',teacherId])
+      this.router.navigate(['/teachers/view-teacher', teacherId])
     }
-  
+
   }
 
 
