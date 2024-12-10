e0b2ca8201a887858bfdb1b4c887b36c84e4e407
akshaymv <Akshaymv.prm@gmail.com>
2024-12-10 13:02:54 +0530

    latest commit

--- src/app/modules/teachers/transfer-request-list/transfer-request-list.component.ts
+++ src/app/modules/teachers/transfer-request-list/transfer-request-list.component.ts
@@ -316,6 +316,7 @@ export class TransferRequestListComponent implements OnInit {
                     approveBtn.innerHTML = '<i class="bi bi-check-lg  " style="font-size:16px"></i>Approve';
                     approveBtn.style.width = '86px';
                     approveBtn.style.paddingRight = "10px"
+                    approveBtn.setAttribute('title','Approve Transfer Request')
 
 
 
@@ -323,6 +324,7 @@ export class TransferRequestListComponent implements OnInit {
                     rejectBtn.classList.add('btn', 'btn-sm', 'btn-outline-danger', 'status-btn');
                     rejectBtn.innerHTML = '<i class="bi bi-x  " style="font-size:16px"></i> Reject';
                     rejectBtn.style.width = '80px';
+                    rejectBtn.setAttribute('title','Reject Transfer Request')
 
                     divSub.appendChild(approveBtn);
                     divSub.appendChild(rejectBtn)
